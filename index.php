fd463fff16b7c07121df7156f9e4df256f235814
miyukii69 <147715889+miyukii69@users.noreply.github.com>
2025-04-29 21:22:56 +0700

    created routes file

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
     require "function.php"; // Include helper functions
-    
-    $uri = parse_url($_SERVER['REQUEST_URI']) ['path']; // Get the current URI
-
-    $routes = [
-        '/PHP/' => 'controllers/index.php',
-        '/PHP/about' => 'controllers/about.php',
-        '/PHP/contact' => 'controllers/contact.php',
-    ];
-    
-    if(array_key_exists($uri,$routes)) {
-        require $routes[$uri];
-    } else {
-
-        http_response_code(404);
-        require 'views/404.php';
-
-        die();
-    }
+    require "router.php";
---
+++ router.php
@@ -0,0 +1,24 @@
+<?php
+    $uri = parse_url($_SERVER['REQUEST_URI']) ['path']; // Get the current URI
+
+    $routes = [
+        '/PHP/' => 'controllers/index.php',
+        '/PHP/about' => 'controllers/about.php',
+        '/PHP/contact' => 'controllers/contact.php',
+    ];
+
+    function routeToController($uri,$routes) {
+        if(array_key_exists($uri,$routes)) {
+            require $routes[$uri];
+        } else {
+            abort();
+        }
+    }
+
+    function abort($code = 404) {
+        http_response_code($code);
+        require "views/{$code}.php";
+
+        die();
+    }
+    routeToController($uri,$routes);
