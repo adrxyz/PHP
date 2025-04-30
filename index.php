aeecce5fa719bcef35db3e3c16f81580c8d38b14
miyukii69 <147715889+miyukii69@users.noreply.github.com>
2025-04-30 13:17:13 +0700

    fetching mysql

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
     require "function.php"; // Include helper functions
-    require "router.php";
+    // require "router.php";
+// connect the mySQL database.
+    $dsn = "mysql:host=localhost;port=3306;dbname=php;charset=utf8mb4"; //for the user=root;password=Jessemontejo261104
+    
+    $pdo =new PDO($dsn, 'root', 'Jessemontejo261104');
+
+    $statement =$pdo -> prepare ("SELECT * FROM users");
+    $statement -> execute();
+
+    $users = $statement -> fetchAll();
+
+    dd($users);
